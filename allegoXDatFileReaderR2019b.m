87b41f9df50dcf5b8c8201fc79e8060036c40271
Francisco J. Flores <fjflores@mit.edu>
2022-08-22 16:46:22 -0400

    format license to keep within editor borders.

--- allegoXDatFileReaderR2019b.m
+++ allegoXDatFileReaderR2019b.m
@@ -13,35 +13,42 @@ function reader = allegoXDatFileReader()
     %   convertToKilosort2Bin(datasourceName, destinationFile, timerange)
     %
     %   timeRange: [1 2] array with the start and end time in seconds
-    %   timeRange is optional for all get*Sigs methods, with the default being the full time range
+    %   timeRange is optional for all get*Sigs methods, with the default 
+    %     being the full time range
     %
-    %   datasourceName: string. The full data source name, including the path & excluding file extensions
-    %   destinationFile: string, optional. The full file name for the output file when converting to kilosort2 format. 
+    %   datasourceName: string. The full data source name, including the 
+    %     path & excluding file extensions
+    %   destinationFile: string, optional. The full file name for the 
+    %     output file when converting to kilosort2 format. 
     %       Default is the datasource+'_kilosort2.bin'
     %
-    %   signalStruct: struct with three fields: signals, timeStamps, timeSamples
-    %   signals: [M N] matrix, where M is the number of channels, and N is the number of time samples
+    %   signalStruct: struct with three fields: signals, timeStamps, 
+    %     timeSamples
+    %   signals: [M N] matrix, where M is the number of channels, and N is 
+    %     the number of time samples
     %   timeStamps: [1 N] array with timeStamp data
     %   timeSamples: [1 N] array with time samples (seconds)
 
     % Copyright (c) 2019 NeuroNexus
     % 
-    % Permission is hereby granted, free of charge, to any person obtaining a copy
-    % of this file and associated documentation files (the "Software"), to deal
-    % in the Software without restriction, including without limitation the rights
-    % to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    % copies of the Software, and to permit persons to whom the Software is
-    % furnished to do so, subject to the following conditions:
+    % Permission is hereby granted, free of charge, to any person obtaining
+    % a copy of this file and associated documentation files (the 
+    % "Software"), to deal in the Software without restriction, including 
+    % without limitation the rights to use, copy, modify, merge, publish, 
+    % distribute, sublicense, and/or sell copies of the Software, and to 
+    % permit persons to whom the Software is furnished to do so, subject 
+    % to the following conditions:
     % 
-    % The above copyright notice and this permission notice shall be included in all
-    % copies or substantial portions of the Software.
+    % The above copyright notice and this permission notice shall be 
+    % included in all copies or substantial portions of the Software.
     % 
-    % THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    % IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    % FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    % AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    % LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    % THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+    % EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+    % MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    % NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+    % BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+    % ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+    % CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     % SOFTWARE.
 
     reader.getAllegoXDatAllSigs=@getAllegoXDatAllSigs;
